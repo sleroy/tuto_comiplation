ab9308ca51da78bb5003b2a180a65e157bd71534
FrancisANDRE <zosrothko@orange.fr>
2016-06-08 08:58:03 +0200

    Use JDK 1.7 instead of JDK 1.8

--- src/main/java/fr/echoeslabs/tuto/jdt/refactoring/SwitchOrderingRefactoring.java
+++ src/main/java/fr/echoeslabs/tuto/jdt/refactoring/SwitchOrderingRefactoring.java
@@ -1,10 +1,17 @@
 package fr.echoeslabs.tuto.jdt.refactoring;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
-import java.util.function.Consumer;
+import java.util.Map;
 
+import javax.swing.text.BadLocationException;
+
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.ToolFactory;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.BreakStatement;
@@ -13,6 +20,12 @@ import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.Statement;
 import org.eclipse.jdt.core.dom.SwitchCase;
 import org.eclipse.jdt.core.dom.SwitchStatement;
+import org.eclipse.jdt.core.formatter.CodeFormatter;
+import org.eclipse.jdt.core.formatter.DefaultCodeFormatterConstants;
+import org.eclipse.jface.text.Document;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.text.edits.MalformedTreeException;
+import org.eclipse.text.edits.TextEdit;
 
 import fr.echoeslabs.tuto.jdt.refactoring.api.AbstractRefactoring;
 import fr.echoeslabs.tuto.jdt.refactoring.api.RefactoringException;
@@ -22,12 +35,9 @@ import fr.echoeslabs.tuto.jdt.refactoring.api.RefactoringException;
  * order.
  */
 public class SwitchOrderingRefactoring extends AbstractRefactoring {
-	List<BreakBlock<SwitchCase, List<Statement>>> cases;
-	BreakBlock<SwitchCase, List<Statement>> breakBlock;
-	List<Statement> caseStatements;
-	
-	static Comparator<? super BreakBlock<SwitchCase, List<Statement>>> comparator = new BreakBlockComparator();
-	
+
+	private static Comparator<? super BreakBlock<SwitchCase, List<Statement>>> comparator = new BreakBlockComparator();
+
 	private final static class SwitchCollector extends ASTVisitor {
 
 		private final List<SwitchStatement> switchStatements = new ArrayList<SwitchStatement>();
@@ -43,19 +53,37 @@ public class SwitchOrderingRefactoring extends AbstractRefactoring {
 		}
 	}
 
-	private static class LexicalOrderComparator implements Comparator<String> {
+	private static class BreakBlockComparator implements Comparator<BreakBlock<SwitchCase, List<Statement>>> {
 
 		@Override
-		public int compare(String o1, String o2) {
-			return 0;
+		public int compare(BreakBlock<SwitchCase, List<Statement>> left,
+				BreakBlock<SwitchCase, List<Statement>> right) {
+			Expression le = left.first().getExpression();
+			Expression re = right.first().getExpression();
+			if (le == null)
+				return +1;
+			if (re == null)
+				return -1;
+			return le.toString().compareTo(re.toString());
 		}
 
 	}
 
-	private class BreakBlockConsumer implements Consumer<Statement> {
-		@Override
-		public void accept(Statement statement) {
-				
+	@SuppressWarnings("unchecked")
+	private void reorder(SwitchStatement switchStatement) {// SwitchCase
+		AST ast = switchStatement.getAST();
+
+		List<BreakBlock<SwitchCase, List<Statement>>> cases;
+		BreakBlock<SwitchCase, List<Statement>> breakBlock = null;
+		List<Statement> caseStatements = null;
+		
+		cases = new ArrayList<BreakBlock<SwitchCase, List<Statement>>>();
+
+		List<Statement> statements = switchStatement.statements();
+		
+		Iterator<Statement> statementIterator = statements.iterator();
+		while (statementIterator.hasNext()) {
+			Statement statement = statementIterator.next();
 			if (statement instanceof SwitchCase) {
 				SwitchCase switchCase = (SwitchCase) statement;
 				if (breakBlock == null) {
@@ -70,47 +98,23 @@ public class SwitchOrderingRefactoring extends AbstractRefactoring {
 			} else
 				caseStatements.add(statement);
 		}
-	}
 
-	private static class BreakBlockComparator implements Comparator<BreakBlock<SwitchCase, List<Statement>>> {
-
-		@Override
-		public int compare(BreakBlock<SwitchCase, List<Statement>> left, BreakBlock<SwitchCase, List<Statement>> right) {
-			Expression le = left.first().getExpression();
-			Expression re = right.first().getExpression();
-			if (le == null)
-				return +1;
-			if (re == null)
-				return -1;
-			return le.toString().compareTo(re.toString());
-		}
-		
-	}
-	
-	private void reorder(SwitchStatement switchStatement) {// SwitchCase
-		AST ast = switchStatement.getAST();
-		cases = new ArrayList<BreakBlock<SwitchCase, List<Statement>>>();
-		List<Statement> statements = switchStatement.statements();
-		Consumer breakBlockConsumer = new BreakBlockConsumer();
-
-		breakBlock = null;caseStatements = null;
-		statements.forEach(breakBlockConsumer);
-		
 		Statement last = caseStatements.get(caseStatements.size() - 1);
 		if (!(last instanceof BreakStatement)) {
 			caseStatements.add(ast.newBreakStatement());
 		}
-		
-		cases.sort(comparator);
-		
-		
+
+		// cases.sort(comparator);
+		Collections.sort(cases, comparator);
+
 		statements.clear();
-		for(BreakBlock<SwitchCase, List<Statement>> kase : cases) {
+		for (BreakBlock<SwitchCase, List<Statement>> kase : cases) {
 			statements.addAll(kase.second());
 		}
 		last = statements.get(statements.size() - 1);
 		if (last instanceof BreakStatement)
 			statements.remove(statements.size() - 1);
+		
 	}
 
 	@Override
@@ -118,11 +122,10 @@ public class SwitchOrderingRefactoring extends AbstractRefactoring {
 		// do not change AST
 		final SwitchCollector switchCollector = new SwitchCollector();
 		inputAST.accept(switchCollector);
-		
+
 		final List<SwitchStatement> switchStatements = switchCollector.getSwitchStatements();
 		for (final SwitchStatement switchStatement : switchStatements) {
 			reorder(switchStatement);
 		}
-
 	}
 }
